88a553839ac18d919ca4ed997490fc98a454463f
Andy Yair Bolaño Castilla <andy_yair@live.com>
2016-05-16 18:42:36 -0500

    ultimo

---
+++ public_html/admin/script/app.js
@@ -0,0 +1,66 @@
+var uri = "../../public";
+var app;
+(function(){
+    app = angular.module("domigo_admin", ['ngRoute','ui.keypress']);
+    
+    app.config(['$routeProvider', '$locationProvider', function AppConfig($routeProvider, $locationProvider){
+            
+            
+            $routeProvider
+            .when("/home",{
+                        templateUrl: 'home.html'
+                    })
+             .when("/mensajeros",{
+                        templateUrl: 'mensajeros.html'
+                    })
+                         
+                    .otherwise({
+                        redirectTo:"/home"
+                    });
+                    
+            
+    }]);
+
+    app.directive('ngEnter', function () {
+        return function (scope, elements, attrs) {
+            elements.bind('keydown keypress', function (event) {
+                if (event.which === 13) {
+                    scope.$apply(function () {
+                        scope.$eval(attrs.ngEnter);
+                    });
+                    event.preventDefault();
+                }
+            });
+        };
+    });
+    
+    app.filter('ifEmpty', function() {
+        return function(input, defaultValue) {
+            if (angular.isUndefined(input) || input === null || input === '') {
+                return defaultValue;
+            }
+
+            return input;
+        };
+    });
+    
+    app.directive('uploaderModel',['$parse',function($parse){
+        return{
+            restrict: 'A',
+            link: function(scope,iElement,iAttrs){
+                iElement.on('change',function(e)
+                {
+                    $parse(iAttrs.uploaderModel).assign(scope,iElement[0].files[0]);
+                });
+            }
+        };
+
+    }]);
+
+   
+
+})();
+
+
+
+
---
+++ public_html/admin/script/controllers/homeController.js
@@ -0,0 +1,12 @@
+app.controller('homeController',['$scope','$http', function ($scope,$http){
+       
+        
+       $scope.new_empresa = function(){
+            $('#newEmpresa').modal('show'); 
+       }
+ 
+    }]); 
+
+
+
+
