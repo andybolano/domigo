3200ded3a8597a245545584a91ee7898a8b16666
Jose M. Soto <jomisoac@gmail.com>
2016-06-09 23:00:13 -0500

    Agregada carpeta gestion de pagos

---
+++ src/app/centrales/gestion-pagos/gestion-pagos.module.js
@@ -0,0 +1,32 @@
+/**
+ * Created by Jose Soto
+ * on 9/06/2016.
+ */
+(function () {
+    'use strict';
+
+    angular
+        .module('app.central_pagos', [])
+        .config(config)
+        .run(run);
+
+    function config($stateProvider) {
+        $stateProvider
+            .state('app.gestion-pagos', {
+                url: '/central/pagos',
+                templateUrl: 'app/centrales/gestion-pagos/central-pagos.html',
+                data: {
+                    onlyAccess: 'EMPRESA'
+                },
+                controller: 'CentralPagosController',
+                controllerAs: 'vm'
+            })
+    }
+
+    function run(appMenu) {
+        appMenu.addTo([
+            {nombre: 'Gestion pagos', link: 'app.gestion-pagos', icon: 'fa fa-diamond'},
+        ], 'EMPRESA');
+    }
+})();
+
