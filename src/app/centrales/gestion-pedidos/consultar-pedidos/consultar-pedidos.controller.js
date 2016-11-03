c35deb3f509990b81f09384e7310440dc6e9ee61
Jose Miguel Soto Acosta <jomisoac@gmail.com>
2016-11-03 13:07:19 -0500

    fix diferencia de fechas

--- src/app/centrales/gestion-pedidos/consultar-pedidos/consultar-pedidos.controller.js
+++ src/app/centrales/gestion-pedidos/consultar-pedidos/consultar-pedidos.controller.js
@@ -33,18 +33,19 @@
 
         function cargarRangoDePedidos() {
             vm.domicilios = [];
-            var desde = $filter('date')(vm.fechaInicio, 'yyyy-MM-dd');
-            var hasta = $filter('date')(vm.fechaFinal, 'yyyy-MM-dd');
-            var aFecha1 = desde.split('-');
-            var aFecha2 = hasta.split('-');
-            var fFecha1 = Date.UTC(aFecha1[2],aFecha1[1]-1,aFecha1[0]);
-            var fFecha2 = Date.UTC(aFecha2[2],aFecha2[1]-1,aFecha2[0]);
-            var dif = fFecha2 - fFecha1;
+            // var desde = $filter('date')(vm.fechaInicio, 'yyyy-MM-dd');
+            // var hasta = $filter('date')(vm.fechaFinal, 'yyyy-MM-dd');
+            // var aFecha1 = desde.split('-');
+            // var aFecha2 = hasta.split('-');
+            // var fFecha1 = Date.UTC(aFecha1[2],aFecha1[1]-1,aFecha1[0]);
+            // var fFecha2 = Date.UTC(aFecha2[2],aFecha2[1]-1,aFecha2[0]);
+            // var dif = fFecha2 - fFecha1;
             // var dias = Math.floor(dif / (1000 * 60 * 60 * 24));
-            if(dif < 0){
+            var dif = diferenciaEntreDiasEnDias(vm.fechaInicio, vm.fechaFinal);
+            if (dif < 0) {
                 toastr.warning('formato de fechas invalido, la fecha hasta debe ser mayor a la fecha desde', 'Espera!');
-            }else{
-                Restangular.service('domicilios?fecha_desde=' + desde + '&fecha_hasta=' + hasta, Restangular.one('empresas', authService.currentUser().empresa.id)).getList().then(function (response) {
+            } else {
+                Restangular.service('domicilios?fecha_desde=' + vm.fechaInicio + '&fecha_hasta=' + vm.fechaFinal, Restangular.one('empresas', authService.currentUser().empresa.id)).getList().then(function (response) {
                     if (response.length <= 0) {
                         toastr.warning('No se registraron domicilios en la fecha o fechas seleccionadas', 'Espera!');
                     } else {
@@ -52,6 +53,14 @@
                     }
                 });
             }
+        }
+
+        function diferenciaEntreDiasEnDias(a, b) {
+            var MILISENGUNDOS_POR_DIA = 1000 * 60 * 60 * 24;
+            var utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+            var utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+
+            return Math.floor((utc2 - utc1) / MILISENGUNDOS_POR_DIA);
         }
 
         function removeDomicilio($index, domicilio) {
@@ -66,7 +75,7 @@
                 confirmButtonText: "Si",
                 closeOnConfirm: true
             }, function () {
-                Restangular.one('domicilios/'+ domicilio.id).remove().then(function (response) {
+                Restangular.one('domicilios/' + domicilio.id).remove().then(function (response) {
                     toastr.success('Has eliminado el domicilio correctamente.', 'Eliminado!');
                     vm.domicilios.splice($index, 1);
                 });
