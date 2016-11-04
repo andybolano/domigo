fe0b131c7f0dd1ed5de1b49258bfdf4c4f2bd1f7
Jose Miguel Soto Acosta <jomisoac@gmail.com>
2016-11-03 22:07:49 -0500

    fix pagos

--- src/app/centrales/gestion-pagos/consultar-pagos.controller.js
+++ src/app/centrales/gestion-pagos/consultar-pagos.controller.js
@@ -23,18 +23,19 @@
         function cargarPagos() {
             vm.pagos = [];
             vm.total = 0;
-            var desde = $filter('date')(vm.fechaInicio, 'yyyy-MM-dd');
-            var hasta = $filter('date')(vm.fechaFinal, 'yyyy-MM-dd');
-            var aFecha1 = desde.split('-');
-            var aFecha2 = hasta.split('-');
-            var fFecha1 = Date.UTC(aFecha1[2],aFecha1[1]-1,aFecha1[0]);
-            var fFecha2 = Date.UTC(aFecha2[2],aFecha2[1]-1,aFecha2[0]);
-            var dif = fFecha2 - fFecha1;
-            var dias = Math.floor(dif / (1000 * 60 * 60 * 24));
-            if(dias < 0){
+            // var desde = $filter('date')(vm.fechaInicio, 'yyyy-MM-dd');
+            // var hasta = $filter('date')(vm.fechaFinal, 'yyyy-MM-dd');
+            // var aFecha1 = desde.split('-');
+            // var aFecha2 = hasta.split('-');
+            // var fFecha1 = Date.UTC(aFecha1[2],aFecha1[1]-1,aFecha1[0]);
+            // var fFecha2 = Date.UTC(aFecha2[2],aFecha2[1]-1,aFecha2[0]);
+            // var dif = fFecha2 - fFecha1;
+            // var dias = Math.floor(dif / (1000 * 60 * 60 * 24));
+            var dif = diferenciaEntreDiasEnDias(vm.fechaInicio, vm.fechaFinal)
+            if(dif < 0){
                 toastr.warning('formato de fechas invalido, la fecha hasta debe ser mayor a la fecha desde', 'Espera!');
             }else{
-                Restangular.service('pagos?fecha_desde=' + desde + '&fecha_hasta=' + hasta, Restangular.one('empresas', authService.currentUser().empresa.id)).getList().then(function (response) {
+                Restangular.service('pagos?fecha_desde=' + vm.fechaInicio + '&fecha_hasta=' + vm.fechaFinal, Restangular.one('empresas', authService.currentUser().empresa.id)).getList().then(function (response) {
                     if (response.length <= 0) {
                         toastr.warning('No se registraron pagos en la fecha o fechas seleccionadas', 'Espera!');
                     } else {
@@ -46,6 +47,14 @@
 
                 });
             }
+        }
+
+        function diferenciaEntreDiasEnDias(a, b) {
+            var MILISENGUNDOS_POR_DIA = 1000 * 60 * 60 * 24;
+            var utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+            var utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+
+            return Math.floor((utc2 - utc1) / MILISENGUNDOS_POR_DIA);
         }
 
         cargarPagosIniciales()
